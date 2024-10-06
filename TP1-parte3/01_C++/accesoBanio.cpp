b82c63181fa0da3f70d2a8427abd993c3f86b3ff
leandro <leandrojuan1998@gmail.com>
2024-10-06 17:27:58 -0400

    Agrego modificacion script banio

--- TP1-parte3/01_C++/accesoBanio.cpp
+++ TP1-parte3/01_C++/accesoBanio.cpp
@@ -46,10 +46,7 @@ void mujeres(int numero)
     }
     accesoRegionCritica.lock();
     cantidadMujeres++;
-    if (cantidadMujeres == 1)
-    {
-        situacionBaño = HAY_MUJERES;
-    }
+    if (cantidadMujeres == 1) { situacionBaño = HAY_MUJERES; }
     imprimirPantalla.lock();
     cout << nombre + " entro al baño." << endl;
     cout << "\t- CantidadHombres: " + to_string(cantidadHombres) + "\n\t- CantidadMujeres: " + to_string(cantidadMujeres) << endl;
